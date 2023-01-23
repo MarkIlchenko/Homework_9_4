fb3985be955facae068fcd4b5058591edf28da2e
Mark Ilchenko <markilchenko18.hillel@gmail.com>
2023-01-23 19:34:35 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+// Homework 4
+
+let myArray = [4, -5, 0, 2, -67, 8, 10, -34 ];
+
+function getNegativeNumbers(array) {
+
+    let negatives = [];
+
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] < 0) {
+            negatives.push(array[i]);
+        }
+    }
+    return negatives;
+
+}
+console.log(getNegativeNumbers(myArray));
